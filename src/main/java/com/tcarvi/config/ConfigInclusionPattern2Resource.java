aac7d3e71b43ec44c16d5fa0539ecdaa6db8e8f2
TCARVI <tcarvi@me.com>
2021-09-20 21:15:19 -0300

    Refactor

--- src/main/java/com/tcarvi/config/ConfigInclusionPattern2Resource.java
+++ src/main/java/com/tcarvi/config/ConfigInclusionPattern2Resource.java
@@ -16,7 +16,7 @@ public class ConfigInclusionPattern2Resource {
     public String getLookupConfigValue() {
         // Padrão 2 para inclusão de propriedade de configuração
         Config config = ConfigProvider.getConfig();
-        String includedValue = config.getValue("value", String.class);
+        String includedValue = config.getValue("included.value", String.class);
         return "Value included by pattern 2 is = " + includedValue;
     }
 }
